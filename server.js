bf90a86203623b31efce2c6cfad959d57449dfb3
vikrant48 <vikrantchauhan9794@gmail.com>
2025-08-25 20:47:26 +0530

    update

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import dotenv from "dotenv"
-import connectDB from "./db/db.js"
-import {app} from "./app.js"
+import connectDB from "./src/db/db.js"
+import {app} from "./src/app.js"
 
 dotenv.config({
-    path: '../.env'
+    path: './.env'
 })
 
 connectDB()
