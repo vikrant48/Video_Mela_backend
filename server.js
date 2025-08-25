afe85cb2960a68e079c3d91955ccdea4d90c7381
vikrant48 <vikrantchauhan9794@gmail.com>
2025-08-25 21:05:04 +0530

    update

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 import dotenv from "dotenv"
 import connectDB from "./src/db/db.js"
 import {app} from "./src/app.js"
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+import process from 'process'
+
+// Ensure we're in the correct directory
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+process.chdir(__dirname)
 
 dotenv.config({
     path: './.env'
