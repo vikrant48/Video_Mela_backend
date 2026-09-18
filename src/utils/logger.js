232a19b95d532366a01a4ff6f82be2ad7c8070ee
vikrant48 <vikrantchauhan9794@gmail.com>
2026-09-18 19:54:03 +0530

    refactor: remove timestamp from logger messages for cleaner output

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,16 +1,14 @@
-const getTimestamp = () => new Date().toISOString();
-
 export const logger = {
     info: (message, ...meta) => {
-        console.log(`[${getTimestamp()}] [INFO] ${message}`, meta.length ? meta : '');
+        console.log(`[INFO] ${message}`, meta.length ? meta : '');
     },
     warn: (message, ...meta) => {
-        console.warn(`[${getTimestamp()}] [WARN] ${message}`, meta.length ? meta : '');
+        console.warn(`[WARN] ${message}`, meta.length ? meta : '');
     },
     error: (message, ...meta) => {
-        console.error(`[${getTimestamp()}] [ERROR] ${message}`, meta.length ? meta : '');
+        console.error(`[ERROR] ${message}`, meta.length ? meta : '');
     },
     http: (message, ...meta) => {
-        console.log(`[${getTimestamp()}] [HTTP] ${message}`, meta.length ? meta : '');
+        console.log(`${message}`, meta.length ? meta : '');
     }
 };
